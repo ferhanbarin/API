1b17e593258b02b97caadb196d6ad8ea4665846f
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-08 23:19:57 +0200

    API

--- src/test/java/API/day04/GetRequest02.java
+++ src/test/java/API/day04/GetRequest02.java
@@ -1,9 +1,8 @@
 package API.day04;
 
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class GetRequest02 {
@@ -14,7 +13,7 @@ public class GetRequest02 {
 */
 
     @Test
-    public void test() {
+    public void test1() {
 
         String URL = "https://restful-booker.herokuapp.com/booking";
 
@@ -23,5 +22,27 @@ public class GetRequest02 {
         response.prettyPrint();
 
         response.then().assertThat().statusCode(200).contentType("application/json");
+    }
+
+    @Test
+    public void test2() {
+/*
+        https://restful-booker.herokuapp.com/booking/1001 URL'ine
+        Accept type'i "application/json" olan GET request'i yolladigimda gelen response'un status kodunun 404 oldugunu ve
+        Response body'sinin "Not Found" icerdigini ve Response body'sinin "API" icermedigini test edin.
+*/
+
+        String URL = "https://restful-booker.herokuapp.com/booking/1001";
+
+        Response response = given().accept("application/json").when().get(URL);
+
+        response.prettyPrint();
+
+        response.then().assertThat().statusCode(404);
+
+        Assert.assertTrue(response.asString().contains("Not Found"));
+        Assert.assertFalse(response.asString().contains("API"));
+
+        // asString() method'u ile JSON formatinda gelen response'yi String'e cevirdik.
     }
 }
