081a88d64cf0153a7424a431720f5f2da86ba15a
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-09 22:52:24 +0200

    API

--- src/test/java/API/day05/GetRequest05.java
+++ src/test/java/API/day05/GetRequest05.java
@@ -1,6 +1,9 @@
 package API.day05;
 
+import io.restassured.response.Response;
 import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class GetRequest05 {
 
@@ -17,6 +20,15 @@ public class GetRequest05 {
     @Test
     public void test() {
 
+        String URL = "http://dummy.restapiexample.com/api/v1/employees";
 
+        Response response = given().accept("application/json").when().get(URL);
+
+        response.prettyPrint();
+
+        response.then().assertThat().statusCode(200).contentType("application/json")
+                .body("data.id", hasSize(24),
+                        "data.employee_name", hasItem("Ashton Cox"),
+                        "data.employee_age", hasItems(21, 23, 61));
     }
 }
