3181fba914d16ee2e1e2738ad62b82367d3a3115
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-09 22:30:32 +0200

    API

---
+++ src/test/java/API/day05/GetRequest05.java
@@ -0,0 +1,22 @@
+package API.day05;
+
+import org.junit.Test;
+
+public class GetRequest05 {
+
+/*
+    http://dummy.restapiexample.com/api/v1/employees URL'ine
+    Accept type'i "application/json" olan GET request'i yolladigimda
+    Gelen response'un
+    Status kodunun 200 ve content type'inin "application/json"
+    Ve employees sayisinin 24
+    Ve employee'lerden birinin "Ashton Cox"
+    Ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin.
+*/
+
+    @Test
+    public void test() {
+
+
+    }
+}
