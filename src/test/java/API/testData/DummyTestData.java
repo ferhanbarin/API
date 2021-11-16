fbbc4cdbe2fa1b1874966cc4c8d67a5c2a533df7
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-16 23:02:47 +0200

    API

---
+++ src/test/java/API/testData/DummyTestData.java
@@ -0,0 +1,35 @@
+package API.testData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DummyTestData {
+
+    public HashMap <String, Object> setUpTestData() {
+
+        List <Integer> yaslar = new ArrayList <Integer>();
+        yaslar.add(40);
+        yaslar.add(21);
+        yaslar.add(19);
+
+
+        HashMap <String, Object> calisanBilgileri11 = new HashMap<String, Object>();
+        calisanBilgileri11.put("id", 11);
+        calisanBilgileri11.put("employee_name", "Jena Gaines");
+        calisanBilgileri11.put("employee_salary", 90560);
+        calisanBilgileri11.put("employee_age", 30);
+        calisanBilgileri11.put("profile_image", "");
+
+
+        HashMap <String, Object> expectedData = new HashMap <String, Object>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("besinciCalisan", "Airi Satou");
+        expectedData.put("calisanSayisi", 24);
+        expectedData.put("sondanIkinciCalisanMaasi", 106450);
+        expectedData.put("arananYaslar", yaslar);
+        expectedData.put("onbirinciCalisan", calisanBilgileri11);
+
+        return expectedData;
+    }
+}
