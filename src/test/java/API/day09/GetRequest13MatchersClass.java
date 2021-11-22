8231c64b768d62900da72e33d4da287dfac2c876
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-22 14:20:23 +0200

    API

--- src/test/java/API/day09/GetRequest13MatchersClass.java
+++ src/test/java/API/day09/GetRequest13MatchersClass.java
@@ -51,7 +51,5 @@ public class GetRequest13MatchersClass extends Dummy {
                         "data[10].employee_salary", equalTo(((Map<?, ?>) expectedDataMap.get("onbirinciCalisan")).get("employee_salary")),
                         "data[10].employee_age", equalTo(((Map<?, ?>) expectedDataMap.get("onbirinciCalisan")).get("employee_age")),
                         "data[10].profile_image", equalTo(((Map<?, ?>) expectedDataMap.get("onbirinciCalisan")).get("profile_image")));
-
-
     }
 }
