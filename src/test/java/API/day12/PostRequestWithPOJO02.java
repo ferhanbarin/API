b2cd421e524c2035c77342a5d1a3f106b6f78b69
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-29 21:04:03 +0200

    API

--- src/test/java/API/day12/PostRequestWithPOJO02.java
+++ src/test/java/API/day12/PostRequestWithPOJO02.java
@@ -1,6 +1,17 @@
 package API.day12;
 
-public class PostRequestWithPOJO02 {
+import API.POJOS.BookingDatesPOJO;
+import API.POJOS.BookingPOJO;
+import API.POJOS.BookingResponsePOJO;
+import API.TestBase.HerokuApp;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostRequestWithPOJO02 extends HerokuApp {
 
 /*
     https://restful-booker.herokuapp.com/booking
@@ -30,5 +41,25 @@ public class PostRequestWithPOJO02 {
                       } oldugunu test edin.
 */
 
+    @Test
+    public void test() {
 
+        spec02.pathParam("parametre1", "booking");
+
+        BookingDatesPOJO bookingdates = new BookingDatesPOJO("2020-09-09", "2020-09-21");
+        BookingPOJO bookingPojo = new BookingPOJO("Selim", "Ak", 15000, true, bookingdates);
+
+        Response response = given().contentType(ContentType.JSON).spec(spec02).auth().basic("admin", "password123").body(bookingPojo).post("/{parametre}");
+        response.prettyPrint();
+
+        BookingResponsePOJO actualData = response.as(BookingResponsePOJO.class);
+
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals(bookingPojo.getFirstname(), actualData.getBooking().getFirstname());
+        Assert.assertEquals(bookingPojo.getLastname(), actualData.getBooking().getLastname());
+        Assert.assertEquals(bookingPojo.getTotalprice(), actualData.getBooking().getTotalprice());
+        Assert.assertEquals(bookingPojo.isDepositpad(), actualData.getBooking().isDepositpad());
+        Assert.assertEquals(bookingPojo.getBookingdates().getCheckin(), actualData.getBooking().getBookingdates().getCheckin());
+        Assert.assertEquals(bookingPojo.getBookingdates().getCheckout(), actualData.getBooking().getBookingdates().getCheckout());
+    }
 }
