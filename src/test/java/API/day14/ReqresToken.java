299a1158315dac735464a70beec35f875519d316
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-11-30 21:00:04 +0200

    API

---
+++ src/test/java/API/day14/ReqresToken.java
@@ -0,0 +1,33 @@
+package API.day14;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import static io.restassured.RestAssured.given;
+
+public class ReqresToken {
+
+    public void tokenAl() {
+
+        String URL = "https://reqres.in/api/login";
+
+        HashMap <String, String> requestBody = new HashMap<String, String>();
+        requestBody.put("email", "eve.holt@reqres.in");
+        requestBody.put("password", "cityslicka");
+
+        System.out.println(requestBody);
+
+        Response response = given().contentType(ContentType.JSON).body(requestBody).when().post(URL);
+        response.prettyPrint();
+
+        JsonPath json = response.jsonPath();
+        String token = json.getString("token");
+        System.out.println(token);
+
+
+
+
+
+    }
+}
